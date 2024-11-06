15b924f4f43614a14c48b2b34b45c70a32051b88
RahmahRamadhani <127211226+RahmahRamadhani@users.noreply.github.com>
2024-11-06 21:13:46 +0700

    .

--- project/backend/controllers/dataController.js
+++
@@ -1,8 +0,0 @@
-exports.getData = (req, res) => {
-    const data = {
-      message: "Data berhasil diambil!",
-      // Tambahkan data lainnya sesuai kebutuhan
-    };
-    res.json(data);
-  };
-  
--- project/backend/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getData } = require('../controllers/dataController');
-
-// Rute untuk mendapatkan data
-router.get('/data', getData);
-
-module.exports = router;
